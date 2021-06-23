6044c30eba936ef829442f3b1067fedac1715bed
gaurav bhatt <gauravdotbhatt@gmail.com>
2021-06-23 13:22:56 +0530

    serialization module.

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,9 @@
+
+from snippets import views
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
+]
