ad9ad3be8956b8681beee8704d46e20e2be43ca8
gaurav bhatt <gauravdotbhatt@gmail.com>
2021-06-23 14:01:58 +0530

    wrapping api views and response

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,9 +1,11 @@
 
 from snippets import views
-from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
